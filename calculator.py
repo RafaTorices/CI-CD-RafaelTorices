26827220a5b0aba33471abc3595f6c0bfa2658a2
RAFAEL TORICES <torices.rafael@gmail.com>
2023-11-15 14:05:50 +0100

    docstring in calculator.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,74 @@
-# calculator.py
+# Calculator.py
 # Python Application for the myapp project.
 # Example Python calculator application
 
+"""
+Calculator functions for doing math operations on numbers.
+"""
 
-# Function to add two numbers
+
 def add(number1, number2):
+    """
+    Function to add two numbers
+
+    Args:
+        number1 (int): First number
+        number2 (int): Second number
+
+    Returns:
+        int: Sum of number1 + number2
+
+    Raises:
+        TypeError: If either number1 or number2 is not an integer
+    """
     return number1 + number2
 
 
-# Function to subtract two numbers
 def subtract(number1, number2):
+    """
+    Function to substract two numbers
+
+    Args:
+        number1 (int): First number
+        number2 (int): Second number
+
+    Returns:
+        int: Substract of number1 - number2
+
+    Raises:
+        TypeError: If either number1 or number2 is not an integer
+    """
     return number1 - number2
 
 
-# Function to multiply two numbers
 def multiply(number1, number2):
+    """
+    Function to multiply two numbers
+
+    Args:
+        number1 (int): First number
+        number2 (int): Second number
+
+    Returns:
+        int: Multiply of number1 * number2
+
+    Raises:
+        TypeError: If either number1 or number2 is not an integer
+    """
     return number1 * number2
 
 
-# Function to divide two numbers
 def divide(number1, number2):
+    """
+    Function to divide two numbers
+
+    Args:
+        number1 (int): First number
+        number2 (int): Second number
+
+    Returns:
+        int: Divide of number1 / number2
+    Raises:
+        TypeError: If either number1 or number2 is not an integer
+    """
     return number1 / number2

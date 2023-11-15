8b2caff55424daf638dd215f18944d2665f49c21
RAFAEL TORICES <torices.rafael@gmail.com>
2023-11-15 14:11:08 +0100

    doctring in test file

--- test_main.py
+++ test_main.py
@@ -1,19 +1,37 @@
 # Test file for calculator.py
 
-# Imports functions to be tested
+"""
+File to test the calculator.py file
+"""
 
 from calculator import add, subtract, multiply, divide
 
 
-class test_calculator:
+class TestCalculator:
+    """
+    Class to test the calculator.py file
+    """
+
     def test_addition(self):
+        """
+        Function to test the add function
+        """
         assert 4 == add(2, 2)
 
     def test_subtraction(self):
+        """
+        Function to test the subtract function
+        """
         assert 2 == subtract(4, 2)
 
     def test_multiplication(self):
+        """
+        Function to test the multiply function
+        """
         assert 100 == multiply(10, 10)
 
     def test_division(self):
+        """
+        Function to test the divide function
+        """
         assert 5 == divide(25, 5)

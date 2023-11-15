fbaf9d0b3ba1915b025c3c802a49fccadd9d1526
RAFAEL TORICES <torices.rafael@gmail.com>
2023-11-15 13:17:44 +0100

    cicd: test

--- calculator.py
+++ calculator.py
@@ -11,3 +11,13 @@ def add(number1, number2):
 # Function to subtract two numbers
 def subtract(number1, number2):
     return number1 - number2
+
+
+# Function to multiply two numbers
+def multiply(number1, number2):
+    return number1 * number2
+
+
+# Function to divide two numbers
+def divide(number1, number2):
+    return number1 / number2
--- test_main.py
+++ test_main.py
@@ -2,12 +2,18 @@
 
 # Imports functions to be tested
 
-from calculator import add, subtract
+from calculator import add, subtract, multiply, divide
 
 
 class test_calculator:
-    def test_add(self):
+    def test_addition(self):
         assert 4 == add(2, 2)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         assert 2 == subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 100 == multiply(10, 10)
+
+    def test_division(self):
+        assert 5 == divide(25, 5)

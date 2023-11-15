5b8510c34cdf200d09207ee8b9cb774d4f4bfe49
RAFAEL TORICES <torices.rafael@gmail.com>
2023-11-15 12:04:04 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# main.py
+# Python Application for the myapp project (main.py)
+# Example Python calculator application
+
+
+# Function to add two numbers
+def add(number1, number2):
+    return number1 + number2
+
+
+# Function to subtract two numbers
+def subtract(number1, number2):
+    return number1 - number2
+
+
+# Function to multiply two numbers
+def multiply(number1, number2):
+    return number1 * number2
+
+
+# Function to divide two numbers
+def divide(number1, number2):
+    return number1 / number2
+
+
+if __name__ == "__main__":
+    result = add(2, 3)
+    print(f"Result: {result}")
---
+++ test_main.py
@@ -0,0 +1,6 @@
+# tests/test_main.py
+from main import add
+
+
+def test_add():
+    assert add(4, 3) == 5

a9926bb22c53e9e408f3f69dad391daad9922ba7
RAFAEL TORICES <torices.rafael@gmail.com>
2023-11-15 13:05:00 +0100

    test only two functions

--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@
 
 # Imports functions to be tested
 
-from calculator import add, subtract, multiply, divide
+from calculator import add, subtract
 
 
 class test_calculator:
@@ -11,9 +11,3 @@ class test_calculator:
 
     def test_subtract(self):
         assert 2 == subtract(4, 2)
-
-    def test_multiply(self):
-        assert 100 == multiply(10, 10)
-
-    def test_divide(self):
-        assert 5 == divide(25, 5)
